a8e2ec46288ff9f991fda1a24f89fcbed5051658
ZuhaibJama <zuhaibjama83@gmail.com>
2022-09-14 12:28:10 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+void print_alphabet(void);
+{
+	char i = 'a' ;
+
+	for ( i <= 'z'; i++)
+	{
+		_putchar(i);
+		i++;
+
+
+	}
+	putchar('\n');
+	
+}
