4a50db0f60ec3d6f67de4ba38ec829a42021cdc2
ZuhaibJama <zuhaibjama83@gmail.com>
2022-09-14 14:27:34 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - alphabet
+ * print_alphabet_x10 - print a - z ten times 
+ */
+
+int _islower(int c);
+{
+	if ( c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+
+	else 
+	{
+		return (0);
+	}
+
+}
