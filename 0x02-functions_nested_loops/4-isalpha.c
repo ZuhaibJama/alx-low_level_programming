9b380bf73eb0840f205570fb1d42a6738d9cb661
ZuhaibJama <zuhaibjama83@gmail.com>
2022-09-14 14:57:17 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - function whether letter is lowercase or uppercase
+ * @c: contains value to be compared
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+
+{
+	char c;
+	{
+		if ((c >= 'a' && c <= 'z') ||  (c >= 'A' && c <= 'Z'))
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
